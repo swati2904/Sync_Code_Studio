a0d645c4d5288e18950b17667b0f124a93956d94
Swati Saxena <swati.lpu17@gmail.com>
2024-03-27 19:59:22 -0700

    Added Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const createnewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("Room ID & username is required");
+      return;
+    }
+
+    //   Redirect to editorpage
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code_sync.png"
+          alt="code-sync-logo"
+        />
+
+        <h4 className="mainLabel"> Paste invitation Room ID</h4>
+
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="Room ID"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            onKeyUp={handleInputEnter}
+          />
+
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="User Name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyUp={handleInputEnter}
+          />
+
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a onClick={createnewRoom} href="" className="createNewBtn">
+              New Room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer className="footer">
+        <h4>
+          Designed with ❤️ by &nbsp;
+          <a
+            href={"https://github.com/swati2904/Sync_Code_Studio"}
+            target="_blank"
+            className="link footer__link"
+            rel="noreferrer"
+          >
+            Swati Saxena
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
 };
 export default Home;
