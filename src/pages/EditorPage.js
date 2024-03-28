fc1dd1793d791448de87b37e41edb8e43b8f0668
Swati Saxena <swati.lpu17@gmail.com>
2024-03-27 22:12:34 -0700

    implemented editor page UI

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return <div>Editor Page</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Swati" },
+    { socketId: 2, username: "Saurav" },
+    { socketId: 3, username: "Shweta" },
+    { socketId: 4, username: "Saxena" },
+  ]);
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img
+              className="logoImage"
+              src="/code_sync.png"
+              alt="code-sync-logo"
+            />
+          </div>
+          <h3> Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+      </div>
+    </div>
+  );
 };
 export default EditorPage;
