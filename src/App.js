03e0a93f5c372ccf57fea1a8544cea092756dd0a
Swati Saxena <swati.lpu17@gmail.com>
2024-03-27 22:10:40 -0700

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#368654,",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
